9b7bd8bc14a58e566cf9d1db2b9243bb28b61788
Dat-Guy <46610971+Dat-Guy@users.noreply.github.com>
2019-04-26 11:55:15 -0400

    Update Main.java

--- src/DatGuy/Main.java
+++ src/DatGuy/Main.java
@@ -8,6 +8,10 @@ ISSUE LIST:
     > Username formatting - Is it the full email, or just the username?
     > Date formatting - Is it in Unix epoch time? HH:MM (AM/PM) MM/DD/YYYY? HH/MM (24-hour) DD/MM/YYYY?
     > What indexes are the data in the CVS file stored at? User at first, print id at 0, etc?
+* Requested features
+    -Monthy goals
+    -Scoreboard (ananomous ranking)
+    -
     
 * The preferred way for the email to be formatted is requested, so this script can format the email, or ALTERNATELY
 give a plugin so data for email recipients and key data can be passed along.
@@ -26,7 +30,6 @@ import java.util.ArrayList;
 public class Main {
 	
 	private static final String FILE_NAME = ""; //Where the CVS file is located
-	private static final Long SECONDS_AGO = (long) 60 * 60 * 24 * 30; //Tells the system to get all emails from the past month (in seconds).
 	
 	private static final char SEPARATOR = ','; //CVS element separator
 	private static final char QUOTE = '\"'; //CVS quotation
@@ -34,8 +37,7 @@ public class Main {
 	//TODO: Insert proper values for 'User Element', the index of element in each print task with the username, and 'Print Qty Element', similar use.
 	
 	private static final int USER_ELEMENT = 0; //User id index
-	private static final int PRINT_QTY_ELEMENT = 1; //Print quantity index
-	private static final int PRINT_DATE_ELEMENT = 2; //Print execution time TODO: Get date formatting.
+	private static final int[] PRINT_QTY_ELEMENT = new int[]{1,2,3}; //Print quantity indexs
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		
@@ -52,24 +54,18 @@ public class Main {
 		
 		for (int i = 0; i < data.size(); i++) {
 			String User = data.get(i).get(USER_ELEMENT); //Grab print task user
-			Long PrintQty = Long.parseUnsignedLong(data.get(i).get(PRINT_QTY_ELEMENT)); //Grab print task quantity
+			Long PrintQty = (long) 0; //Grab print task quantity
 			
-			String RawDate = data.get(i).get(PRINT_DATE_ELEMENT); //Grab the raw date element
-			
-			//TODO: Using Data formatting, process RawDate into number of seconds before start of script
-			
-			Long SecondsAgo = (long) 0; //Set the number of seconds beforehand information was printed
-			
-			//If the number of seconds ago the print happened falls within the filer...
-			if (SecondsAgo < SECONDS_AGO) {
+			for (var j = 0; j < PRINT_QTY_ELEMENT.length; j++){
+				PrintQty += Long.parseUnsignedInt(data.get(i).get(PRINT_QTY_ELEMENT[j]));	
+			}
 				
-				//If the user is already defined (a previous print by the same user already been processed)
-				if (Users.indexOf(User) != -1) {
-					PrintCount.set(Users.indexOf(User), PrintQty + PrintCount.get(Users.indexOf(User))); //Add to the existing print count.
-				} else {
-					Users.add(User); //Add new user entry
-					PrintCount.add(PrintQty); //Add new print quantity
-				}
+			//If the user is already defined (a previous print by the same user already been processed)
+			if (Users.indexOf(User) != -1) {
+				PrintCount.set(Users.indexOf(User), PrintQty + PrintCount.get(Users.indexOf(User))); //Add to the existing print count.
+			} else {
+				Users.add(User); //Add new user entry
+				PrintCount.add(PrintQty); //Add new print quantity
 			}
 		}
 		
